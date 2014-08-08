d5fcbb46bc3b92b3d71cc830f0b678c0d5a4f693
Tina <tina@jordan.(none)>
2014-08-07 19:55:05 -0700

    adds check4Winner(), creates syntax error at connect4.js:163

--- js/connect4.js
+++ js/connect4.js
@@ -1,14 +1,26 @@
 var fieldSizeX = 7;
 var fieldSizeY = 6;
-var field = new Array();
+var field = [];
 //~ field = [[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7]];
 
+function createBoardAndField() {
+	for (var i = 0; i < fieldSizeY; i++) {
+		$( ".container-fluid" ).append( "<div class='row rowNum-" + i +"'></div>" );
+		var fieldRow = new Array();
+		for (var j = 0; j < fieldSizeX; j++) {
+			var num = (i*7)+j;
+			$( ".rowNum-" + i ).append( "<div class='col-xs-1 colNum-" + j + " pocket-" + i +"-" + j + "'></div>" );
+			fieldRow.push('_');
+		}
+		console.log("building row " + i);
+		field[i] = fieldRow;
+	}
+}
 function writeField() {
 	for (var row = 0; row < fieldSizeY; row++) {
 		console.log(row + '|' + field[row] + '|');
-	};
-};
-
+	}
+}
 function drawField() {
 	for (var i = 0; i < fieldSizeY; i++) {
 		for (var j = 0; j < fieldSizeX; j++) {
@@ -20,28 +32,13 @@ function drawField() {
 					var color = 'black';
 					break;
 				case "o":
-					var color = 'red'
+					var color = 'red';
 					break;
-			};
+			}
 			$( ".pocket-"+i+"-"+j ).addClass(color);
-		};
-	};
-};
-
-function createBoard() {
-	for (var i = 0; i < fieldSizeY; i++) {
-		$( ".container-fluid" ).append( "<div class='row rowNum-" + i +"'></div>" );
-		var fieldRow = new Array();
-		for (var j = 0; j < fieldSizeX; j++) {
-			var num = (i*7)+j;
-			$( ".rowNum-" + i ).append( "<div class='col-xs-1 colNum-" + j + " pocket-" + i +"-" + j + "'></div>" );
-			fieldRow.push('_');
-		};
-		console.log("building row " + i);
-		field[i] = fieldRow;
-	};
-};
-
+		}
+	}
+}
 function dropToken(column, team) {
 	for (var row = field.length; row > 0; row--) {
 		if (field[row - 1][column] === '_') {
@@ -58,7 +55,7 @@ function checkWinner() {
 			if (field[row][column] === field[row][column + 1] === field[row][column + 2] === field[row][column + 3]) {
 				if (field[row][column] === 'x') {
 					gameOver = 1;
-					console.log(gameOver + " = gameOver")
+					console.log(gameOver + " = gameOver");
 					winner = 'X';
 					return winner;
 				}
@@ -124,24 +121,95 @@ function checkWinner() {
 	}
 };
 
+function checkWinnerUp(x,y) {
+	var thisToken = field[x][y];
+	for (var i = 1; i < 4; i++) {
+		if (field[x - i][y] !== thisToken) return false;
+	}
+	return true;
+};
+
+function checkWinnerRight(x,y) {
+	var thisToken = field[x][y];
+	for (var i = 1; i < 4; i++) {
+		if (field[x][y + i] !== thisToken) return false;
+	}
+	return true;
+};
+
+
+function checkWinnerUpRight(x,y) {
+	var thisToken = field[x][y];
+	for (var i = 1; i < 4; i++) {
+		if (field[x - i][y + i] !== thisToken) return false;
+	}
+	return true;
+};
+
+
+function checkWinnerUpLeft(x,y) {
+	var thisToken = field[x][y];
+	for (var i = 1; i < 4; i++) {
+		if (field[x - i][y - i] !== thisToken) return false;
+	}
+	return true;
+};
+
+function check4Winner() {
+	for (var y = field.length; y > 0; y++) {
+		for (var x = field[y].length; x > 0; x--) {
+			var goodUpwards = (y > 2);
+			var goodToRight = (x < (field[y].length - 3));
+			var goodToLeft = (x > 2));
+			var thisTeam = field[x][y];
+			if (goodUpwards) {
+				if (checkWinnerUp(x,y)) {
+					gameOver(thisTeam);
+				};
+			};
+			if (goodToRight) {
+				if (checkWinnerRight(x,y)) {
+					gameOver(thisTeam);
+				};
+			};
+			if (goodToRight && goodUpwards) {
+				if (checkWinnerUpRight(x,y)) {
+					gameOver(thisTeam);
+				};
+			};
+			if (goodUpwards && goodToLeft) {
+				if (checkWinnerUpLeft(x,y)) {
+					gameOver(thisTeam);
+				};
+			};
+		};
+	};
+};
+
+function gameOver(team) {
+	gameOver = true;
+	winner = team;
+};
+
 function connect4() {
 	console.log('Welcome to Connect 4!');
-	var gameOver = 0;
+	var gameOver = false;
 	var winner = '';
 	while (!gameOver) {
 		writeField();
 		drawField();
-		checkWinner();
+		check4Winner();
 		console.log("It's X's turn!");
 		var turn1 = eval(prompt('Drop token in which column? (0-6)'));
 		dropToken(turn1, 'x');
 		writeField();
 		drawField();
-		checkWinner();
+		check4Winner();
 		console.log("It's O's turn!");
 		var turn2 = eval(prompt('Drop token in which column? (0-6)'));
 		dropToken(turn2, 'o');
 	}
-	console.log(checkWinner())
+	alert(winner + "wins the game!")
+	console.log(winner + " wins the game!")
 };
 
--- js/scene.js
+++ js/scene.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	//generate 6 rows of 7 collumns of col-xs-1s
 	//Numbered from top left(0) to bottom right(41)
-	createBoard();
+	createBoardAndField();
+
+	//play connect4
 	connect4();
 
 });
